b2ddfdf4e051925037a361c529587312d8e5a806
Dani <drincianu.dani@gmail.com>
2022-07-27 15:33:41 +0300

    Lesson 6 assignment

---
+++ 6.arraysCollectionsAndGenerics/ConsoleApp1/GenericCollection.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class GenericCollection<T>
+    {
+        private readonly int maxSize;
+        private int index;
+        private T[] items;
+
+        public GenericCollection(int maxSize = 10)
+        {
+            this.maxSize = maxSize;
+            this.items = new T[maxSize];
+            this.index = 0;
+        }
+
+        public void SetItemAtIndex(T item, int index)
+        {
+            if(index >= maxSize) 
+            { 
+                throw new InvalidOperationException("Cannot insert item at this index"); 
+            }
+            items[index] = item;
+        }
+
+        public T GetItemAtIndex(int index)
+        {
+            if(index < 0 || index >= maxSize)
+            {
+                throw new InvalidOperationException("Index out of bounds"); 
+            }
+            else
+            {
+                return items[index];
+            }
+
+        }
+
+        //item swap
+        public void Swap(ref T item1, ref T item2)
+        {
+            T temp = item1;
+            item1 = item2;
+            item2 = temp;
+        }
+
+        //index swap
+        public void Swap(int index1, int index2)
+        {
+            T temp = items[index1];
+            items[index1] = items[index2];
+            items[index2] = temp;
+        }
+
+        //index and item swap
+        public void Swap(int index1, ref T item2)
+        {
+            int index2 = Array.IndexOf(items, item2);
+            T temp = items[index1];
+            items[index1] = items[index2];
+            items[index2] = temp;
+        }
+     
+    }
+}
---
+++ 6.arraysCollectionsAndGenerics/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using ConsoleApp1;
+
+try
+{
+    var stringArray = new GenericCollection<string>();
+
+    stringArray.SetItemAtIndex("aa", 0);
+    stringArray.SetItemAtIndex("bb", 1);
+    stringArray.SetItemAtIndex("cc", 9);
+    var item1 = stringArray.GetItemAtIndex(0);
+    var item2 = stringArray.GetItemAtIndex(9);
+    Console.WriteLine("Before swap");
+    Console.WriteLine(item1);
+    Console.WriteLine(item2);
+
+    //item swap
+    stringArray.Swap(ref item1, ref item2);
+    Console.WriteLine("After index swap");
+    Console.WriteLine(item1);
+    Console.WriteLine(item2);
+
+
+    //index swap
+    item1 = stringArray.GetItemAtIndex(0);
+    item2 = stringArray.GetItemAtIndex(9);
+    stringArray.Swap(0, 9);
+    Console.WriteLine("After item swap");
+    Console.WriteLine(item1);
+    Console.WriteLine(item2);
+
+    //index and item swap
+    item1 = stringArray.GetItemAtIndex(0);
+    item2 = stringArray.GetItemAtIndex(9);
+    stringArray.Swap(0, ref item2);
+    Console.WriteLine("After index and item swap");
+    Console.WriteLine(item1);
+    Console.WriteLine(item2);
+}
+catch (InvalidOperationException e)
+{
+    Console.WriteLine(e.Message);
+}
