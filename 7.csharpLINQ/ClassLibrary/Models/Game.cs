e2806422cfa92525f922bb96ca85bc10ead553cb
Dani <drincianu.dani@gmail.com>
2022-07-28 16:54:32 +0300

    More queries

---
+++ 7.csharpLINQ/ClassLibrary/Models/Game.cs
@@ -0,0 +1,76 @@
+﻿using GameZone.Domain.Exceptions;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameZoneModels 
+{
+    public class Game
+    {
+        private static int serial = 1;
+        public int id { get; set; }
+        public string name { get; set; }
+        public DateTime releaseDate { get; set; }
+        public double totalRating { get; set; }
+        public string gameDetails { get; set; }
+        public List<Developer> Developers { get; set; }
+        public List<Genre> Genres { get; set; }
+        public List<Platform> Platforms { get; set; }
+        public List<Review> Reviews { get; set; }
+        public List<Comment> Comments { get; set; }
+        public Game(string name, DateTime releaseDate, string gameDetails)
+        {
+            this.name = name;
+            this.releaseDate = releaseDate;
+            this.gameDetails = gameDetails;
+            this.id = serial++;
+        }
+
+        public void CalculateTotalRating()
+        {
+            totalRating = Reviews.Average(review => review.rating);
+        }
+        public static Game ReturnGameById(List<Game> gameslist, int id)
+        {
+            try
+            {
+                Game gameToReturn = null;
+                foreach (var game in gameslist)
+                {
+                    gameToReturn = gameslist.Where(game => game.id == id).FirstOrDefault();
+                }
+                return gameToReturn;
+            }
+            catch (NullReferenceException)
+            {
+                throw new NullReferenceException($"Game with id {id} doesn't exist.");
+            }
+        }
+
+        public void AddDeveloperToGameByID(List<Developer> developers, int id)
+        {
+            try
+            {
+                var developerToAdd = developers.Where(developer => developer.id == id).FirstOrDefault();
+                Developers.Add(developerToAdd);
+            } catch (NullReferenceException)
+            {
+                throw new NullReferenceException($"Developer with {id} doesn't exist.");
+            }
+        }
+
+        public static List<Game> GenerateTopList(List<Game> gameList)
+        {
+            try
+            { 
+                return gameList.OrderByDescending(game => game.totalRating).ToList();
+            } catch (NullReferenceException)
+            {
+                throw new NullReferenceException($"Game list is null");
+            }
+        }
+    }
+}
---
+++ 7.csharpLINQ/ClassLibrary/Models/User.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameZoneModels
+{
+    public class User
+    {
+        private static int serial = 1;
+        public int id { get; set; }
+        public string? username { get; set; }
+        public string? email { get; set; }
+        public string? password { get; set; }
+        public string? firstName { get; set; }
+        public string? lastName { get; set; }
+        public List<Game> FavoriteGames { get; set; }
+        public List<Review> userReviews { get; set; }
+
+        public User(string email, string username, string password, string firstName, string lastName)
+        {
+            this.id = serial++;
+            this.email = email;
+            this.username = username;
+            this.password = password;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            FavoriteGames = new List<Game>();
+            userReviews = new List<Review>();
+        }
+        public void Login()
+        {
+            Console.WriteLine("User logged in");
+        }
+
+        public void Logout()
+        {
+            Console.WriteLine("User logged out");
+        }
+
+        public void PostReview(Game gameToBeReviewed, double rating, string content)
+        {
+            Review review = new Review(this, gameToBeReviewed, rating, content);
+            gameToBeReviewed.Reviews.Add(review);
+            gameToBeReviewed.CalculateTotalRating();
+            userReviews.Add(review);
+        }
+        public void AddGameToFavorite(Game favoriteGame)
+        {
+            FavoriteGames.Add(favoriteGame);
+        }
+        public void PostComment(Game gameToBeCommented, string content)
+        {
+            gameToBeCommented.Comments.Add(new Comment(this, content));
+        }
+        public void ReplyToComment(Game gameToBeReplied, int commentToReplyID, string content)
+        {
+            //find commentById 
+            try
+            {
+                var commentToReply = gameToBeReplied.Comments.Where(comment => comment.id == commentToReplyID).FirstOrDefault();
+                commentToReply.replies.Add(new Reply(this, commentToReply, content));
+            }
+            catch (NullReferenceException)
+            {
+                throw new NullReferenceException($"Comment with id: {commentToReplyID} does not exist.");
+            }
+        }
+        public void DeleteComment(Game gameToDeleteComment, int id)
+        {
+            try
+            {
+                var commentToDelete = gameToDeleteComment.Comments.Where(comment => comment.id == id).FirstOrDefault();
+                gameToDeleteComment.Comments.Remove(commentToDelete);
+            }
+            catch (NullReferenceException)
+            {
+                throw new NullReferenceException($"Comment with id: {id} does not exist.");
+            }
+        }
+
+        public Game CreateGame(string name, int year, int month, int day, string gameDetails)
+        {
+            var game = new Game(name, new DateTime(year, month, day), gameDetails);
+            game.Developers = new List<Developer> {};
+            game.Genres = new List<Genre> {};
+            game.Platforms = new List<Platform> {};
+            game.Reviews = new List<Review>();
+            game.Comments = new List<Comment>();
+            return game;
+        }
+    }
+}
