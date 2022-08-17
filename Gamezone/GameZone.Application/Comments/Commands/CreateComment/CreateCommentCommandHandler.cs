7f797f2a14b61fd08522d802cf9ea46e58a5f59b
Dani <drincianu.dani@gmail.com>
2022-08-17 15:33:22 +0300

    Simplyfing create comment command

--- Gamezone/GameZone.Application/Comments/Commands/CreateComment/CreateCommentCommandHandler.cs
+++ Gamezone/GameZone.Application/Comments/Commands/CreateComment/CreateCommentCommandHandler.cs
@@ -8,28 +8,21 @@ namespace GameZone.Application.Comments.Commands.CreateComment
     public class CreateCommentCommandHandler : IRequestHandler<CreateCommentCommand, CommentDto>
     {
         private readonly ICommentRepository _commentRepository;
-        private readonly IGameRepository _gameRepository;
-        private readonly IUserRepository _userRepository;
         private readonly IMapper _mapper;
 
-        public CreateCommentCommandHandler(ICommentRepository commentRepository, IMapper mapper, IGameRepository gameRepository, IUserRepository userRepository)
+        public CreateCommentCommandHandler(ICommentRepository commentRepository, IMapper mapper)
         {
             _commentRepository = commentRepository;
             _mapper = mapper;
-            _gameRepository=gameRepository;
-            _userRepository=userRepository;
         }
 
         public async Task<CommentDto> Handle(CreateCommentCommand request, CancellationToken cancellationToken)
         {
-            var user = await _userRepository.ReturnByIdAsync(request.UserId);
-            var game = await _gameRepository.ReturnByIdAsync(request.GameId);
-            var userDto = _mapper.Map<User>(user);
-            var gameDto = _mapper.Map<Game>(game);
-            var comment = new Comment { User = userDto, Game= gameDto, Content = request.Content };
-            await _commentRepository.CreateAsync(comment);
+            var commentToAdd = new Comment { UserId = request.UserId, GameId= request.GameId, Content = request.Content };
+            await _commentRepository.CreateAsync(commentToAdd);
+            var getComment = await _commentRepository.ReturnByIdAsync(commentToAdd.Id);
+            var commentDto = _mapper.Map<CommentDto>(getComment);
 
-            var commentDto = _mapper.Map<CommentDto>(comment);
             return commentDto;
         }
     }
