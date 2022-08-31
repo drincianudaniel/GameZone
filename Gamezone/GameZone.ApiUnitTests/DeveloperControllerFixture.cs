4bba2047dca7a1fb337c2032987fa3a2611aa81f
Dani <drincianu.dani@gmail.com>
2022-08-31 14:20:15 +0300

    Game and developers api unit tests

---
+++ Gamezone/GameZone.ApiUnitTests/DeveloperControllerFixture.cs
@@ -0,0 +1,208 @@
+﻿using AutoMapper;
+using GameZone.Api.AutoMapperProfiles;
+using GameZone.Api.Controllers;
+using GameZone.Api.DTOs;
+using GameZone.Api.ViewModels;
+using GameZone.Application.Developers.Commands.CreateDeveloper;
+using GameZone.Application.Developers.Commands.DeleteDeveloper;
+using GameZone.Application.Developers.Queries.GetDeveloperById;
+using GameZone.Application.Developers.Queries.GetDevelopersList;
+using GameZone.Domain.Models;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GameZone.ApiUnitTests
+{
+    public class DeveloperControllerFixture
+    {
+        private readonly Mock<IMediator> _mockMediator = new Mock<IMediator>();
+        private readonly Mock<IMapper> _mockMapper = new Mock<IMapper>();
+        private readonly Mock<ILogger<DevelopersController>> _mockLogger = new Mock<ILogger<DevelopersController>>();
+        private static IMapper _mapper;
+        public DeveloperControllerFixture()
+        {
+            if (_mapper == null)
+            {
+                var mappingConfig = new MapperConfiguration(mc =>
+                {
+                    mc.AddProfile(new DeveloperProfile());
+                });
+                IMapper mapper = mappingConfig.CreateMapper();
+                _mapper = mapper;
+            }
+        }
+        [Fact]
+        public async Task Get_All_Developers_GetAllDevelopersListQueryIsCalled()
+        {
+            //Arrange
+            _mockMediator
+                .Setup(m => m.Send(It.IsAny<GetDevelopersListQuery>(), It.IsAny<CancellationToken>()))
+                .Verifiable();
+
+            //Act
+            var controller = new DevelopersController(_mockMediator.Object, _mapper, _mockLogger.Object);
+            await controller.GetDevelopers();
+
+            //Assert
+            _mockMediator.Verify(x => x.Send(It.IsAny<GetDevelopersListQuery>(), It.IsAny<CancellationToken>()), Times.Once());
+        }
+
+        [Fact]
+        public async Task Get_Developer_By_Id_GetDeveloperByIdQueryIsCalled()
+        {
+            //Arange
+            _mockMediator
+                .Setup(m => m.Send(It.IsAny<GetDeveloperByIdQuery>(), It.IsAny<CancellationToken>()))
+                .Verifiable();
+
+            //Act
+            var controller = new DevelopersController(_mockMediator.Object, _mapper, _mockLogger.Object);
+            await controller.GetById(new Guid());
+
+            _mockMediator.Verify(x => x.Send(It.IsAny<GetDeveloperByIdQuery>(), It.IsAny<CancellationToken>()), Times.Once());
+        }
+
+        [Fact]
+        public async Task Get_Developer_By_Id_GetDeveloperByIdQueryWithCorrectDeveloperIdIsCalled()
+        {
+            //Arrange
+            var developerId = new Guid();
+
+            _mockMediator
+               .Setup(m => m.Send(It.IsAny<GetDeveloperByIdQuery>(), It.IsAny<CancellationToken>()))
+               .Returns<GetDeveloperByIdQuery, CancellationToken>(async (q, c) =>
+               {
+                   developerId = q.Id;
+                   return await Task.FromResult(
+                       new Developer
+                       {
+                           Id = q.Id,
+                           Name = "Ubisoft",
+                           Headquarters = "Montreal"
+                       });
+               });
+
+            //Act
+            var controller = new DevelopersController(_mockMediator.Object, _mapper, _mockLogger.Object);
+            var guid = new Guid("3fefe639-af6a-46f7-b7ca-db1608ec3f65");
+            await controller.GetById(guid);
+
+            //Assert
+            Assert.Equal(developerId, guid);
+        }
+
+        [Fact]
+        public async Task Get_Developer_By_Id_ShouldReturnOkStatusCode()
+        {
+            //Arrange
+            var guid = new Guid("3fefe639-af6a-46f7-b7ca-db1608ec3f65");
+            _mockMediator
+             .Setup(m => m.Send(It.IsAny<GetDeveloperByIdQuery>(), It.IsAny<CancellationToken>()))
+             .ReturnsAsync(
+                new Developer
+                {
+                    Id = guid,
+                    Name = "Ubisoft",
+                    Headquarters = "Montreal"
+                });
+
+            //Act
+            var controller = new DevelopersController(_mockMediator.Object, _mapper, _mockLogger.Object);
+            var result = await controller.GetById(guid);
+            var okResult = result as OkObjectResult;
+            //Assert
+            Assert.Equal((int)HttpStatusCode.OK, okResult.StatusCode);
+        }
+
+        [Fact]
+        public async Task Get_Developer_By_Id_ShouldReturnFoundPlatform()
+        {
+            //Arange
+            var guid = new Guid("3fefe639-af6a-46f7-b7ca-db1608ec3f65");
+            var developer = new Developer
+            {
+                Id = guid,
+                Name= "Ubisoft",
+                Headquarters = "Montreal"
+            };
+
+            var mappedDeveloper = _mapper.Map<DeveloperDto>(developer);
+
+            _mockMediator
+                .Setup(m => m.Send(It.IsAny<GetDeveloperByIdQuery>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(developer);
+            //Act
+            var controller = new DevelopersController(_mockMediator.Object, _mapper, _mockLogger.Object);
+            var result = await controller.GetById(guid);
+            var okResult = result as OkObjectResult;
+
+            //Assert
+            //Assert.Same(mappedGenre, (GenreDto)okResult.Value);
+            //Assert.True(mappedGenre.Equals((GenreDto)okResult.Value));
+            Assert.Equal(mappedDeveloper.Id, ((DeveloperDto)okResult.Value).Id);
+            Assert.Equal(mappedDeveloper.Name, ((DeveloperDto)okResult.Value).Name);
+            Assert.Equal(mappedDeveloper.Headquarters, ((DeveloperDto)okResult.Value).Headquarters);
+        }
+
+        [Fact]
+        public async Task CallPost_ReturnsDeveloperDto()
+        {
+            //Arrange
+            var createDeveloperCommand = new DeveloperViewModel
+            {
+                Name = "Ubisoft",
+                HeadQuarters = "Montreal"
+            };
+
+            _mockMediator
+                .Setup(m => m.Send(It.IsAny<CreateDeveloperCommand>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new Developer
+                {
+                    Name = "Ubisoft",
+                    Headquarters = "Montreal"
+                });
+
+            //Act
+            var controller = new DevelopersController(_mockMediator.Object, _mapper, _mockLogger.Object);
+            var result = await controller.CreateDeveloper(createDeveloperCommand);
+            var createdAtActionResult = result as CreatedAtActionResult;
+            //var genre = createdAtActionResult.Value;
+
+            //Assert
+            Assert.Equal(createDeveloperCommand.Name, ((DeveloperDto)createdAtActionResult.Value).Name);
+            Assert.Equal(createDeveloperCommand.HeadQuarters, ((DeveloperDto)createdAtActionResult.Value).Headquarters);
+        }
+
+        [Fact]
+        public async Task Delete_Platform_Should_Return_No_Content()
+        {
+            //Arrange
+            var guid = new Guid("3fefe639-af6a-46f7-b7ca-db1608ec3f65");
+
+            var platform = new Developer
+            {
+                Id = guid,
+                Name="Ubisoft",
+                Headquarters = "Montreal"
+            };
+
+            _mockMediator
+             .Setup(m => m.Send(It.IsAny<DeleteDeveloperCommand>(), It.IsAny<CancellationToken>()))
+             .ReturnsAsync(platform.Id);
+
+            //Act
+            var controller = new DevelopersController(_mockMediator.Object, _mapper, _mockLogger.Object);
+            var result = await controller.DeleteDeveloper(guid);
+            var noContentResult = result as NoContentResult;
+            //Assert
+            Assert.Equal((int)HttpStatusCode.NoContent, noContentResult.StatusCode);
+        }
+    }
+}
---
+++ Gamezone/GameZone.ApiUnitTests/GameControllerFixture.cs
@@ -0,0 +1,232 @@
+﻿using AutoMapper;
+using GameZone.Api.AutoMapperProfiles;
+using GameZone.Api.Controllers;
+using GameZone.Api.DTOs;
+using GameZone.Api.ViewModels;
+using GameZone.Application.Games.Commands.CreateGame;
+using GameZone.Application.Games.Commands.DeleteGame;
+using GameZone.Application.Games.Queries.GetGameById;
+using GameZone.Application.Games.Queries.GetGamesList;
+using GameZone.Domain.Models;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GameZone.ApiUnitTests
+{
+    public class GameControllerFixture
+    {
+        private readonly Mock<IMediator> _mockMediator = new Mock<IMediator>();
+        private readonly Mock<IMapper> _mockMapper = new Mock<IMapper>();
+        private readonly Mock<ILogger<GamesController>> _mockLogger = new Mock<ILogger<GamesController>>();
+        private static IMapper _mapper;
+        public GameControllerFixture()
+        {
+            if (_mapper == null)
+            {
+                var mappingConfig = new MapperConfiguration(mc =>
+                {
+                    mc.AddProfile(new GameProfile());
+                    mc.AddProfile(new DeveloperProfile());
+                    mc.AddProfile(new GenreProfile());
+                    mc.AddProfile(new PlatformProfile());
+                });
+                IMapper mapper = mappingConfig.CreateMapper();
+                _mapper = mapper;
+            }
+        }
+        [Fact]
+        public async Task Get_All_Games_GetAllGamesListQueryIsCalled()
+        {
+            //Arrange
+            _mockMediator
+                .Setup(m => m.Send(It.IsAny<GetGameListQuery>(), It.IsAny<CancellationToken>()))
+                .Verifiable();
+
+            //Act
+            var controller = new GamesController(_mapper, _mockMediator.Object, _mockLogger.Object);
+            await controller.GetGames();
+
+            //Assert
+            _mockMediator.Verify(x => x.Send(It.IsAny<GetGameListQuery>(), It.IsAny<CancellationToken>()), Times.Once());
+        }
+
+        [Fact]
+        public async Task Get_Game_By_Id_GetGamesByIdQueryIsCalled()
+        {
+            //Arange
+            _mockMediator
+                .Setup(m => m.Send(It.IsAny<GetGameByIdQuery>(), It.IsAny<CancellationToken>()))
+                .Verifiable();
+
+            //Act
+            var controller = new GamesController(_mapper, _mockMediator.Object, _mockLogger.Object);
+            await controller.GetById(new Guid());
+
+            _mockMediator.Verify(x => x.Send(It.IsAny<GetGameByIdQuery>(), It.IsAny<CancellationToken>()), Times.Once());
+        }
+
+        [Fact]
+        public async Task Get_Game_By_Id_GetGameByIdQueryWithCorrectGameIdIsCalled()
+        {
+            //Arrange
+            var genreId = new Guid();
+            
+            _mockMediator
+               .Setup(m => m.Send(It.IsAny<GetGameByIdQuery>(), It.IsAny<CancellationToken>()))
+               .Returns<GetGameByIdQuery, CancellationToken>(async (q, c) =>
+               {
+                   genreId = q.Id;
+                   return await Task.FromResult(
+                       new Game
+                       {
+                           Id = q.Id,
+                           Name = "Assassins Creed"
+                       });
+               });
+
+            //Act
+            var controller = new GamesController(_mapper, _mockMediator.Object, _mockLogger.Object);
+            var guid = new Guid("3fefe639-af6a-46f7-b7ca-db1608ec3f65");
+            await controller.GetById(guid);
+
+            //Assert
+            Assert.Equal(genreId, guid);
+        }
+
+        [Fact]
+        public async Task Get_Genre_By_Id_ShouldReturnOkStatusCode()
+        {
+            //Arrange
+            var guid = new Guid("3fefe639-af6a-46f7-b7ca-db1608ec3f65");
+            _mockMediator
+             .Setup(m => m.Send(It.IsAny<GetGameByIdQuery>(), It.IsAny<CancellationToken>()))
+             .ReturnsAsync(
+                new Game
+                {
+                    Id = guid,
+                    Name = "Assassins creed"
+                });
+
+            //Act
+            var controller = new GamesController(_mapper, _mockMediator.Object, _mockLogger.Object);
+            var result = await controller.GetById(guid);
+            var okResult = result as OkObjectResult;
+            //Assert
+            Assert.Equal((int)HttpStatusCode.OK, okResult.StatusCode);
+        }
+
+        [Fact]
+        public async Task Get_Game_By_Id_ShouldReturnFoundGame()
+        {
+            //Arange
+            var guid = new Guid("3fefe639-af6a-46f7-b7ca-db1608ec3f65");
+            var game = new Game
+            {
+                Id = guid,
+                Name="Assassins Creed"
+            };
+
+            var mappedGame = _mapper.Map<GameDto>(game);
+
+            _mockMediator
+                .Setup(m => m.Send(It.IsAny<GetGameByIdQuery>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(game);
+            //Act
+            var controller = new GamesController(_mapper, _mockMediator.Object, _mockLogger.Object);
+            var result = await controller.GetById(guid);
+            var okResult = result as OkObjectResult;
+
+            //Assert
+            //Assert.Same(mappedGenre, (GenreDto)okResult.Value);
+            //Assert.True(mappedGenre.Equals((GenreDto)okResult.Value));
+            Assert.Equal(mappedGame.Name, ((GameDto)okResult.Value).Name);
+        }
+
+        [Fact]
+        public async Task CallPost_ReturnsGameDto()
+        {
+            var genre = new Genre
+            {
+                Id = new Guid("3256165c-e08a-4d17-a4c7-bb5d01a2f982"),
+                Name = "Action"
+            };
+            var developer = new Developer
+            {
+                Id = new Guid("945a91c1-0d5d-4a39-82c9-02a75111a89d"),
+                Name = "Ubisoft",
+                Headquarters = "Montreal"
+            };
+            var platform = new Platform
+            {
+                Id = new Guid("e79c2cad-7117-414a-8fdc-70d83c01b4dc"),
+                Name = "PlayStation 4"
+            };
+
+            //Arrange
+            var createGameCommand = new GameViewModel
+            {
+                Name = "Assassin's Creed",
+                GameDetails = "Assassin's Creed is an action-adventure video game developed by Ubisoft Montreal and published by Ubisoft. It is the first installment in the Assassin's Creed series. The game was released for PlayStation 3 and Xbox 360 in November 2007.",
+                ReleaseDate = new DateTime(2011, 2, 16),
+                ImageSrc = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcHNm4Py0J8lgox2kPl87ZTV4aRjRcIZcq5hyBZX8q&s",
+                GenreList = new List<Guid>{ genre.Id },
+                DeveloperList = new List<Guid>{ developer.Id },
+                PlatformList = new List<Guid>{ platform.Id }
+            };
+
+            _mockMediator
+                .Setup(m => m.Send(It.IsAny<CreateGameCommand>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new Game
+                {
+                    Name = "Assassin's Creed",
+                    GameDetails = "Assassin's Creed is an action-adventure video game developed by Ubisoft Montreal and published by Ubisoft. It is the first installment in the Assassin's Creed series. The game was released for PlayStation 3 and Xbox 360 in November 2007.",
+                    ReleaseDate = new DateTime(2011, 2, 16),
+                    ImageSrc = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcHNm4Py0J8lgox2kPl87ZTV4aRjRcIZcq5hyBZX8q&s",
+                    Developers = new List<Developer> { developer },
+                    Genres = new List<Genre> { genre },
+                    Platforms = new List<Platform> { platform },
+                });
+
+            //Act
+            var controller = new GamesController(_mapper, _mockMediator.Object, _mockLogger.Object);
+            var result = await controller.CreateGame(createGameCommand);
+            var createdAtActionResult = result as CreatedAtActionResult;
+            //var genre = createdAtActionResult.Value;
+
+            //Assert
+            Assert.Equal(createGameCommand.Name, ((GameDto)createdAtActionResult.Value).Name);
+        }
+
+        [Fact]
+        public async Task Delete_Game_Should_Return_No_Content()
+        {
+            //Arrange
+            var guid = new Guid("3fefe639-af6a-46f7-b7ca-db1608ec3f65");
+
+            var game = new Game
+            {
+                Id = guid,
+                Name="Assassin's Creed"
+            };
+
+            _mockMediator
+             .Setup(m => m.Send(It.IsAny<DeleteGameCommand>(), It.IsAny<CancellationToken>()))
+             .ReturnsAsync(game.Id);
+
+            //Act
+            var controller = new GamesController(_mapper, _mockMediator.Object, _mockLogger.Object);
+            var result = await controller.DeleteGame(guid);
+            var noContentResult = result as NoContentResult;
+            //Assert
+            Assert.Equal((int)HttpStatusCode.NoContent, noContentResult.StatusCode);
+        }
+    }
+}
